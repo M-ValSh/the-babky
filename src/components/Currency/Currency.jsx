9366e8c7c01098600da3324cf6774e2c01136ee3
Roman Kotsiurko <kotsiurko@gmail.com>
2023-01-28 21:46:03 +0200

    add styles for tablet and mobile

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -3,12 +3,27 @@ import { bankApi } from 'index';
 // import { fetchCurrency } from '../../services/fetchCurrency';
 import { useEffect, useState } from 'react';
 import { Box } from '@chakra-ui/react';
-// import { useMedia } from 'components/Media/useMedia';
-import { TableWrapperDesc, THdesc, TableText, TR } from './Currency.styled';
+import { useMedia } from 'components/Media/useMedia';
+import {
+  TableWrapperDesc,
+  THDesc,
+  TableTextDesc,
+  TRDesc,
+  TableWrapperTablet,
+  THTablet,
+  TableTextTablet,
+  TRTablet,
+  TableWrapperMobile,
+  THMobile,
+  TableTextMobile,
+  TRMobile,
+} from './Currency.styled';
 import CurrencyDesc from '../../assets/images/iMac-currency.svg';
+import CurrencyTablet from '../../assets/images/tablet-currency.svg';
+import CurrencyMobile from '../../assets/images/mobile-currency.svg';
 
 export const Currency = () => {
-  // const media = useMedia();
+  const media = useMedia();
 
   const [currency, setCurrency] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -47,47 +62,137 @@ export const Currency = () => {
 
   return (
     <>
-      <Box>
-        <TableWrapperDesc bg={CurrencyDesc}>
-          <table>
-            <thead>
-              <TR>
-                <THdesc>Currency</THdesc>
-                <THdesc>Purchase</THdesc>
-                <THdesc>Sale</THdesc>
-              </TR>
-            </thead>
-            <tbody>
-              {isLoading && (
-                <tr>
-                  <td>Please wait...</td>
-                </tr>
-              )}
-              {error && (
-                <tr>
-                  <td colSpan="3">
-                    Too many requests. Sorry, but try again later
-                  </td>
-                </tr>
-              )}
+      {media.isDesktop && (
+        <Box>
+          <TableWrapperDesc bg={CurrencyDesc}>
+            <table>
+              <thead>
+                <TRDesc>
+                  <THDesc>Currency</THDesc>
+                  <THDesc>Purchase</THDesc>
+                  <THDesc>Sale</THDesc>
+                </TRDesc>
+              </thead>
+              <tbody>
+                {isLoading && (
+                  <tr>
+                    <TableTextDesc>Please wait...</TableTextDesc>
+                  </tr>
+                )}
+                {error && (
+                  <tr>
+                    <TableTextDesc colSpan="3">
+                      Too many requests. Sorry, but try again later
+                    </TableTextDesc>
+                  </tr>
+                )}
 
-              {currency &&
-                currency.map(({ currencyCodeA, rateBuy, rateSell }) => {
-                  const codeCurrency = currencyCodeA === 978 ? 'EUR' : 'USD';
-                  const rateBuyCurrency = Number(rateBuy).toFixed(2);
-                  const rateSellCurrency = Number(rateSell).toFixed(2);
-                  return (
-                    <TR key={currencyCodeA}>
-                      <TableText>{codeCurrency}</TableText>
-                      <TableText>{rateBuyCurrency}</TableText>
-                      <TableText>{rateSellCurrency}</TableText>
-                    </TR>
-                  );
-                })}
-            </tbody>
-          </table>
-        </TableWrapperDesc>
-      </Box>
+                {currency &&
+                  currency.map(({ currencyCodeA, rateBuy, rateSell }) => {
+                    const codeCurrency = currencyCodeA === 978 ? 'EUR' : 'USD';
+                    const rateBuyCurrency = Number(rateBuy).toFixed(2);
+                    const rateSellCurrency = Number(rateSell).toFixed(2);
+                    return (
+                      <TRDesc key={currencyCodeA}>
+                        <TableTextDesc>{codeCurrency}</TableTextDesc>
+                        <TableTextDesc>{rateBuyCurrency}</TableTextDesc>
+                        <TableTextDesc>{rateSellCurrency}</TableTextDesc>
+                      </TRDesc>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </TableWrapperDesc>
+        </Box>
+      )}
+
+      {media.isTablet && (
+        <Box>
+          <TableWrapperTablet bg={CurrencyTablet}>
+            <table>
+              <thead>
+                <TRTablet>
+                  <THTablet>Currency</THTablet>
+                  <THTablet>Purchase</THTablet>
+                  <THTablet>Sale</THTablet>
+                </TRTablet>
+              </thead>
+              <tbody>
+                {isLoading && (
+                  <tr>
+                    <TableTextTablet>Please wait...</TableTextTablet>
+                  </tr>
+                )}
+                {error && (
+                  <tr>
+                    <TableTextTablet colSpan="3">
+                      Too many requests. Sorry, but try again later
+                    </TableTextTablet>
+                  </tr>
+                )}
+
+                {currency &&
+                  currency.map(({ currencyCodeA, rateBuy, rateSell }) => {
+                    const codeCurrency = currencyCodeA === 978 ? 'EUR' : 'USD';
+                    const rateBuyCurrency = Number(rateBuy).toFixed(2);
+                    const rateSellCurrency = Number(rateSell).toFixed(2);
+                    return (
+                      <TRTablet key={currencyCodeA}>
+                        <TableTextTablet>{codeCurrency}</TableTextTablet>
+                        <TableTextTablet>{rateBuyCurrency}</TableTextTablet>
+                        <TableTextTablet>{rateSellCurrency}</TableTextTablet>
+                      </TRTablet>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </TableWrapperTablet>
+        </Box>
+      )}
+
+      {media.isMobile && (
+        <Box>
+          <TableWrapperMobile bg={CurrencyMobile}>
+            <table>
+              <thead>
+                <TRMobile>
+                  <THMobile>Currency</THMobile>
+                  <THMobile>Purchase</THMobile>
+                  <THMobile>Sale</THMobile>
+                </TRMobile>
+              </thead>
+              <tbody>
+                {isLoading && (
+                  <tr>
+                    <TableTextMobile>Please wait...</TableTextMobile>
+                  </tr>
+                )}
+                {error && (
+                  <tr>
+                    <TableTextMobile colSpan="3">
+                      Too many requests. Sorry, but try again later
+                    </TableTextMobile>
+                  </tr>
+                )}
+
+                {currency &&
+                  currency.map(({ currencyCodeA, rateBuy, rateSell }) => {
+                    const codeCurrency = currencyCodeA === 978 ? 'EUR' : 'USD';
+                    const rateBuyCurrency = Number(rateBuy).toFixed(2);
+                    const rateSellCurrency = Number(rateSell).toFixed(2);
+                    return (
+                      <TRMobile key={currencyCodeA}>
+                        <TableTextMobile>{codeCurrency}</TableTextMobile>
+                        <TableTextMobile>{rateBuyCurrency}</TableTextMobile>
+                        <TableTextMobile>{rateSellCurrency}</TableTextMobile>
+                      </TRMobile>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </TableWrapperMobile>
+        </Box>
+      )}
     </>
   );
 };
--- src/components/Currency/Currency.styled.js
+++ src/components/Currency/Currency.styled.js
@@ -1,6 +1,4 @@
 import styled from '@emotion/styled';
-// import { useMedia } from 'components/Media/useMedia';
-// const media = useMedia();
 
 export const TableWrapperDesc = styled.div(({ theme, bg }) => {
   return ({
@@ -13,26 +11,106 @@ export const TableWrapperDesc = styled.div(({ theme, bg }) => {
   })
 });
 
-export const TR = styled.tr`
+export const TRDesc = styled.tr`
   max-width: 393px;
   display: flex;
   justify-content: space-around;
   padding: 0px 16px;
 `;
 
-export const THdesc = styled.th(({ theme }) => ({
+export const THDesc = styled.th(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
   fontSize: "18px",
   fontWeight: "700",
   padding: "17px 0",
 
-  width: '393px',
+  width: '131px',
   height: '60px',
 
 }));
 
-export const TableText = styled.td`
-  text-align: center;
-  font-weight: 400;
-  font-size: 16px;
-  padding: 20px 0px 4px;
+export const TableTextDesc = styled.td(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
+  textAlign: "center",
+  fontWeight: "400",
+  fontSize: "16px",
+  padding: "20px 0px 4px",
+}));
+
+// ============================
+// FOR TABLET
+// ============================
+export const TableWrapperTablet = styled.div(({ theme, bg }) => {
+  return ({
+    width: '336px',
+    height: '182px',
+    color: theme.colors.textSecondary,
+    borderRadius: theme.radii.currency,
+    background: `url(${bg}) no-repeat bottom`,
+    margin: "62px 18px",
+  })
+});
+
+export const TRTablet = styled.tr`
+  max-width: 336px;
+  display: flex;
+  justify-content: space-around;
 `;
+
+export const THTablet = styled.th(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
+  fontSize: "18px",
+  lineHeight: "1.5",
+  fontWeight: "700",
+  padding: "11px 0",
+
+  width: '112px',
+}));
+
+export const TableTextTablet = styled.td(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
+  textAlign: "center",
+  fontWeight: "400",
+  fontSize: "16px",
+  lineHeight: "1.5",
+  padding: "16px 0px 4px",
+}));
+
+// ============================
+// FOR MOBILE
+// ============================
+export const TableWrapperMobile = styled.div(({ theme, bg }) => {
+  return ({
+    width: '280px',
+    height: '174px',
+    color: theme.colors.textSecondary,
+    borderRadius: theme.radii.currency,
+    background: `url(${bg}) no-repeat bottom`,
+    margin: "142px 18px",
+  })
+});
+
+export const TRMobile = styled.tr`
+  max-width: 280px;
+`;
+
+export const THMobile = styled.th(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
+  fontSize: "18px",
+  lineHeight: "1.5",
+  fontWeight: "700",
+  padding: "0px 0px 0px 15px",
+
+  width: '93px',
+  height: '50px',
+
+}));
+
+export const TableTextMobile = styled.td(({ theme }) => ({
+  fontFamily: theme.fonts.secondary,
+  textAlign: 'center',
+  fontWeight: "400",
+  fontSize: "16px",
+  lineHeight: "1.5",
+  padding: "12px 0px 0px 5px",
+}));
