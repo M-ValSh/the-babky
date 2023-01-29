6fbf3778ed2128ef778e56e580e9ccb48b56bb49
vitaliybelov.bvs <vitaliybelov.bvs@gmail.com>
2023-01-29 02:35:18 +0200

    add: Logo.jsx & Header: logo & name & button

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -1,18 +1,29 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Box, Button, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Logo } from 'components/Logo/Logo';
 
 export const Headers = () => {
   return (
-    <Grid
-      templateAreas={`"header header"`}
-      gridTemplateRows={'60px 1fr'}
-      gridTemplateColumns={'150px 1fr'}
-      gap="0.5"
-      color="blackAlpha.700"
-      fontWeight="bold"
-    >
-      <GridItem pl="2" bg="orange.300" area={'header'}>
-        Header
-      </GridItem>
-    </Grid>
+    <Box p='20px 0' pl='16px' bgColor='textSecondary'>
+      <Flex alignItems='center'>
+        <Logo />
+        <Box display='flex' alignItems='center' gap='24px' ml='auto' mr='16px' fontFamily='secondary' fontSize='18px' fontWeight='400' color='placeholder'>
+          <Text>Name</Text>
+          <Button >Exit</Button>
+        </Box>
+      </Flex>
+    </Box>
+    // <Grid
+    //   templateAreas={`"header header"`}
+    //   gridTemplateRows={'60px 1fr'}
+    //   gridTemplateColumns={'150px 1fr'}
+    //   gap="0.5"
+    //   color="blackAlpha.700"
+    //   fontWeight="bold"
+    // >
+    //   {/* <GridItem pl="2" bg="orange.300" area={'header'}>
+    //     Header
+    //   </GridItem> */}
+    //   <Logo />
+    // </Grid>
   );
 };
--- src/components/Modals/LogOutModal/LogOutModal.jsx
+++ src/components/Modals/LogOutModal/LogOutModal.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { closeModalWindow } from "redux/global/global-slice";
 
 
@@ -25,8 +25,8 @@ const LogOutModal = () => {
     
 
   return (
-    <div onClick={() => onBackdropClick(e)} className={styles.overlay}>
-        <div className={styles.modal}>
+    <div onClick={() => onBackdropClick(e)} /* className={styles.overlay} */>
+        <div /* className={styles.modal} */>
             <p>Are you sure, you want to Log Out?</p>
             <button type="button" onClick={() => dispatch(closeModalWindow())}>Keep In</button>
             <button type="button" >Log Out</button>
