9ee6dabb599dae580f36f54472d938c111b69465
Irina <iargina.irina@gmail.com>
2023-01-31 23:33:22 +0200

    addTranzaction button style, logout style, stat page style

--- src/components/AddTrnsctnBtn/AddTrnsctnBtn.styled.js
+++ src/components/AddTrnsctnBtn/AddTrnsctnBtn.styled.js
@@ -6,4 +6,12 @@ export const AddTransactionButton = styled.button(({ theme }) => ({
   right: '20px',
   backgroundColor: theme.colors.BgBtnGreen,
   zIndex: '100',
+  color: theme.colors.whiteColor,
+  boxShadow: '0px 6px 15px rgba(36, 204, 167, 0.5)',
+  borderRadius: '50px',
+  width: '40px',
+  height: '40px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 }));
