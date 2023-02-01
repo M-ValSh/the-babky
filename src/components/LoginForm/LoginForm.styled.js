01d161c4ce777b76b94de9d20fdb559e3dbc5c2e
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2023-02-01 15:51:51 +0200

    styles login

--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import pink_ellipse from '../../img/figures/pink_ellipse.svg';
 import purple_ellipse from '../../img/figures/purple_ellipse.svg';
 // import Frame from '../../img/illustrations/Frame.svg';
+import BublePurp from 'assets/images/ellipse-purple.svg';
+import BublRed from 'assets/images/ellipse-pink.svg';
+import { theme } from 'Styles/theme';
 
 export const LoginFormPage = styled.div(({ theme }) => ({
   margin: '0',
@@ -9,7 +12,7 @@ export const LoginFormPage = styled.div(({ theme }) => ({
   gap: '38px',
   backgroundColor: theme.colors.mainBgUnderColor,
   backgroundRepeat: 'no-repeat, no-repeat',
-  backgroundImage: `url(${pink_ellipse}), url(${purple_ellipse})`,
+  backgroundImage: `url(${BublRed}), url(${BublePurp})`,
   backgroundPosition: 'top -150px right -150px, bottom -150px left -150px',
 }));
 
@@ -27,16 +30,6 @@ export const IllustrationWrapper = styled.div(({ theme }) => ({
   flexDirection: 'column',
   justifyContent: 'center',
   paddingTop: '150px',
-  // width: '440px',
-  // width: '550px',
-  // paddingTop: '150px',
-  // paddingLeft: '76px',
-
-  // background: `url(${Frame})`,
-  // backgroundRepeat: 'no-repeat',
-  // backgroundPosition: 'center',
-  // backgroundSize: 'contain',
-  // paddingBottom: '78px',
 }));
 
 export const FormWrapper = styled.div(({ theme }) => ({
@@ -55,13 +48,14 @@ export const FormWrapper = styled.div(({ theme }) => ({
 
 export const IllustrationDesc = styled.p(({ theme }) => ({
   marginTop: '28px',
-  padding: 0,
+  padding: '0',
   textAlign: 'center',
   fontFamily: theme.fonts.main,
   fontSize: '30px',
   lineHeight: '45px',
   color: theme.colors.textMain,
-  fontWeight: 400,
+  fontWeight: '400',
+  zIndex: '10',
 }));
 
 export const FormBlock = styled.div(({ theme }) => ({
@@ -82,4 +76,106 @@ export const FormBlock = styled.div(({ theme }) => ({
   color: theme.colors.textMain,
   //   borderRadius: '20px',
   borderRadius: theme.radii.currency,
+}));
+
+export const FormBlockMobile = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+
+  // margin: '136px 91px 116px 107px',
+  // marginLeft: 'auto',
+  // marginRight: 'auto',
+  // padding: '40px 61px 62px 61px',
+
+  minWidth: '280px',
+  height: '568px',
+
+  alignItems: 'center',
+  //   justifyContent: 'center',
+  backgroundColor: theme.colors.whiteColor,
+  color: theme.colors.textMain,
+  //   borderRadius: '20px',
+  borderRadius: theme.radii.currency,
+}));
+
+export const FormWrapperMobile = styled.div(({ theme }) => ({
+  //   marginLeft: '32px',
+  display: 'block',
+  width: '100%',
+  //   marginLeft: 'auto',
+  //   justifyContent: 'center',
+  //   width: '435px',
+  //   height: '420px',
+
+  backgroundColor: theme.colors.mainBgLighter,
+  backdropFilter: 'blur(25px)',
+  //   blur: '25px',
+}));
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 360px;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background-color: rgba(0, 0, 0, 0.25); */
+
+  /* overflow-y: hidden; */
+  z-index: 100;
+
+  @media (min-width: 768px) {
+    min-width: 603px;
+    min-height: 540px;
+    top: 0;
+  }
+`;
+
+export const BubbleWrapper = styled.div(({ theme }) => ({
+  width: '100%',
+  height: '100vh',
+  minHeight: '800px',
+  zIndex: 1,
+  background: theme.colors.BgMain,
+  '::after': {
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    zIndex: 0,
+    content: '""',
+    // filter: 'blur(25px)',
+    background: `url('${BublePurp}') bottom left no-repeat, url('${BublRed}') top right no-repeat `,
+  },
+}));
+
+export const FormBlockTab = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  padding: '40px 61px 62px 61px',
+
+  width: '532px',
+
+  alignItems: 'center',
+
+  backgroundColor: theme.colors.whiteColor,
+  color: theme.colors.textMain,
+
+  borderRadius: theme.radii.currency,
+}));
+
+export const IllustrationWrapperTab = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  paddingTop: '60px',
+  paddingLeft: '114px',
+  position: 'absolute',
 }));
